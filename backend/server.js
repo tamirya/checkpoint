29c4b7557df2a7b8ba8054d8a9e17a5dff6dd5fe
tamirya2206 <tamir.yakar@gmail.com>
2019-09-07 15:06:40 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,15 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
 
     const url = req.query.checkUrl;
+    const mimeTypeClient = req.query.mimeTypeClient;
 
-    if(!url){
-        return  res.json({msg:'No URL'});
+    if (!url) {
+        return res.json({msg: 'No URL'});
     }
 
     const checkUrl = url.includes('https') ? https : http;
@@ -24,31 +25,31 @@ app.get('/', function (req, res) {
 
         // A chunk of data has been recieved.
         resp.on('data', (chunk) => {
-            if(fileType(chunk)){
+            if (fileType(chunk)) {
                 // check for Executable Files
-                const checkFile =  fileType(chunk);
-                if(checkFile.mime.includes('application')){
+                const checkFile = fileType(chunk);
+                if (checkFile.mime.includes('application')) {
                     isFileExecutable = true;
-                }else{
-                    isFileExecutable=false;
                 }
                 resp.destroy();
+            }else {
+                isFileExecutable = mimeTypeClient.includes('application');
             }
         });
-        
-      // The whole response has been received. Print out the result.
+
+        // The whole response has been received. Print out the result.
         resp.on('end', () => {
-            if(isFileExecutable){
-                return res.json({msg:'Not Allowed'});
-            }else{
-                return res.json({msg:'Allowed'});
+            if (isFileExecutable) {
+                return res.json({msg: 'Not Allowed'});
+            } else {
+                return res.json({msg: 'Allowed'});
             }
         });
-      
-      }).on("error", (err) => {
+
+    }).on("error", (err) => {
         return res.json("Error: " + err.message);
-      });
+    });
 
 });
- 
+
 app.listen(3000);
